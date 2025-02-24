6812160f9a023e9e40c3efef66bf4f3d69caabe0
Peter Heywood <peethwd@gmail.com>
2025-02-24 17:29:44 +0000

    Dynamic version with editbale install support
    -Switch from a fixed version in pyptroject.toml, to a version defined in _verison.py.
    - Adds polychron.__version__ which
      - Respects the curent version on disk, rather than at last package install for editable installs
      - is postfixed with '.dev' to mark as an editable install
      - if editable, git is available and the git hash could be quireied, the most recent committ hash and potentially '.dirty' status are provided as a local version
    
    PEP440 compliant dev/dirty versions

---
+++ src/polychron/_version.py
@@ -0,0 +1,69 @@
+import importlib.metadata
+import json
+import subprocess
+from pathlib import Path
+
+__version__ = "0.2.0"
+
+
+def is_editable_install():
+    """Check if the package has been installed in editable mode or not using importlib."""
+    try:
+        # Get metadata for the installed package and read direct_url.json to check if an editable install
+        distribution = importlib.metadata.distribution("polychron")
+        direct_url = json.loads(distribution.read_text("direct_url.json"))
+        is_editable = direct_url.get("dir_info", {}).get("editable", False)
+        return is_editable
+    except importlib.metadata.PackageNotFoundError:
+        pass
+    return False
+
+
+def get_local_install_git_hash():
+    """Get the short git commit hash for the package, if git is installed and the repository is a local install."""
+    try:
+        # Query package distribution info, to check if editable and find local path
+        distribution = importlib.metadata.distribution("polychron")
+        direct_url = json.loads(distribution.read_text("direct_url.json"))
+        local_path = direct_url.get("url", None)
+        # Return early if not a local filepath, or it does not exist.
+        if not local_path.startswith("file://"):
+            return None
+        package_path = Path(local_path[7:])
+        if not package_path.is_dir():
+            return None
+        # Get the short commit hash
+        result = subprocess.run(
+            ["git", "rev-parse", "--short", "HEAD"],
+            capture_output=True,
+            text=True,
+            check=True,
+            cwd=package_path,
+        )
+        git_hash = result.stdout.strip()
+        # Check if the working directory is dirty
+        result = subprocess.run(
+            ["git", "diff", "--shortstat"],
+            capture_output=True,
+            text=True,
+            check=True,
+            cwd=package_path,
+        )
+        is_dirty = bool(result.stdout.strip())
+        # If the git repo is dirty (i.e. uncommitted changes) postfix the short hash local version identifier with -dirty
+        if is_dirty:
+            git_hash += ".dirty"
+
+        return git_hash
+
+    except subprocess.CalledProcessError:
+        return None
+
+
+if is_editable_install():
+    __version__ = f"{__version__}.dev"
+    git_hash = get_local_install_git_hash()
+    if git_hash is not None:
+        __version__ = f"{__version__}+{git_hash}"
+else:
+    __version__ = __version__
