4b68b603a179f3c35a0875bb547e91c74c321d6c
Peter Heywood <peethwd@gmail.com>
2025-03-11 17:51:26 +0000

    Split popupWindow5 to own file

--- src/polychron/gui.py
+++ src/polychron/gui.py
@@ -36,6 +36,7 @@ from .popupWindow import popupWindow
 from .popupWindow2 import popupWindow2
 from .popupWindow3 import popupWindow3
 from .popupWindow4 import popupWindow4
+from .popupWindow5 import popupWindow5
 from .util import (
     StdoutRedirector,
     clear_all,
@@ -98,22 +99,6 @@ class MainFrame(tk.Tk):
     def get_page(self, page_class):
         '''Shows the desired frame'''
         return self.frames[page_class]
-
-class popupWindow5(object):
-    def __init__(self,master):
-        top=self.top=tk.Toplevel(master)
-        top.configure(bg ='white')
-        self.top.geometry("1000x400")
-        self.top.title("Removal of context")
-        self.l=tk.Label(top,text="Why are you removing this context from your stratigraphy?", bg ='white', font='helvetica 12', fg = '#2f4858')
-        self.l.place(relx = 0.3, rely = 0.1)
-        self.e=tk.Text(top, font='helvetica 12', fg = '#2f4858')
-        self.e.place(relx = 0.3, rely = 0.2, relheight= 0.5, relwidth = 0.5)
-        self.b=tk.Button(top,text='OK',command=self.cleanup,  bg = '#2F4858', font = ('Helvetica 12 bold'),  fg = '#eff3f6')
-        self.b.place(relx = 0.3, rely = 0.7)
-    def cleanup(self):
-        self.value=self.e.get('1.0', 'end')
-        self.top.destroy()
         
 class popupWindow6(object):
     def __init__(self,master):
---
+++ src/polychron/popupWindow5.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+class popupWindow5(object):
+    def __init__(self,master):
+        top=self.top=tk.Toplevel(master)
+        top.configure(bg ='white')
+        self.top.geometry("1000x400")
+        self.top.title("Removal of context")
+        self.l=tk.Label(top,text="Why are you removing this context from your stratigraphy?", bg ='white', font='helvetica 12', fg = '#2f4858')
+        self.l.place(relx = 0.3, rely = 0.1)
+        self.e=tk.Text(top, font='helvetica 12', fg = '#2f4858')
+        self.e.place(relx = 0.3, rely = 0.2, relheight= 0.5, relwidth = 0.5)
+        self.b=tk.Button(top,text='OK',command=self.cleanup,  bg = '#2F4858', font = ('Helvetica 12 bold'),  fg = '#eff3f6')
+        self.b.place(relx = 0.3, rely = 0.7)
+    def cleanup(self):
+        self.value=self.e.get('1.0', 'end')
+        self.top.destroy()
