a17b59d12cbcda65d69ae3bf2244453c156df01d
Peter Heywood <peethwd@gmail.com>
2025-03-11 17:53:44 +0000

    Split popupWindow7 to own file

--- src/polychron/gui.py
+++ src/polychron/gui.py
@@ -38,6 +38,7 @@ from .popupWindow3 import popupWindow3
 from .popupWindow4 import popupWindow4
 from .popupWindow5 import popupWindow5
 from .popupWindow6 import popupWindow6
+from .popupWindow7 import popupWindow7
 from .util import (
     StdoutRedirector,
     clear_all,
@@ -101,37 +102,6 @@ class MainFrame(tk.Tk):
         '''Shows the desired frame'''
         return self.frames[page_class]
         
-class popupWindow7(object):
-    def __init__(self,master, df):
-        top=self.top=tk.Toplevel(master)
-        self.canvas = tk.Canvas(top, bg = 'white')
-        top.title("Data preview")
-        self.canvas.pack()
-        self.l=tk.Label(self.canvas, text="Data Preview")
-        self.l.pack()
-        cols = list(df.columns)
-        
-        tree = ttk.Treeview(self.canvas)
-        tree.pack()
-        tree["columns"] = cols
-        for i in cols:
-            tree.column(i, anchor="w")
-            tree.heading(i, text=i, anchor='w')
-        
-        for index, row in df.iterrows():
-            tree.insert("",0,text=index,values=list(row))
-        tree['show'] = 'headings'
-        self.b=tk.Button(top,text='Load data',command=self.cleanup1,  bg = '#2F4858', font = ('Helvetica 12 bold'),  fg = '#eff3f6')
-        self.b.pack()
-        self.c=tk.Button(top,text='Cancel',command=self.cleanup2,  bg = '#2F4858', font = ('Helvetica 12 bold'),  fg = '#eff3f6')
-        self.c.pack()
-    def cleanup1(self):
-        self.value= 'load'
-        self.top.destroy()
-    def cleanup2(self):
-        self.value='cancel'
-        self.top.destroy()
- 
 
 class popupWindow8(object):
      def __init__(self, master, path):
