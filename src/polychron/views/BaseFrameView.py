febab711db9f6fd59c9cabe3a77f738bdef777fc
Peter Heywood <peethwd@gmail.com>
2025-04-22 18:20:49 +0100

    comment tweak

--- src/polychron/views/BaseFrameView.py
+++ src/polychron/views/BaseFrameView.py
@@ -8,7 +8,7 @@ class BaseFrameView(tk.Frame):
 
     @todo - should this contain a Frame rahter than extend one? I.e. prevent tkinter leaking out of the view
 
-    @todo - check waht the type hint for Parent should be. Parent could be a top level, or a tk.Tk or antother frame? Update in child classes
+    @todo - check waht the type hint for Parent should be. Parent could be a top level, or a tk.Tk or antother frame? Update in child classes. `Misc | None` seems to be used by tk.Frame(master: ) https://github.com/python/typeshed/blob/bc19a28c0dd4876788bd9a5a0deedc20211cd9af/stdlib/tkinter/__init__.pyi#L978
     """
 
     def __init__(self, parent: tk.Frame):
