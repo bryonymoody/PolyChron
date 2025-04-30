c2e6b0ec29bf9bb947582abf94ca2f609f13d1ff
Peter Heywood <peethwd@gmail.com>
2025-04-30 15:56:10 +0100

    gui.py: Delete fully refactored MainFrame class

--- src/polychron/gui.py
+++ src/polychron/gui.py
@@ -813,33 +813,6 @@ class popupWindow3(object):
         write_dot(self.graphcopy, 'fi_new_chrono')
         self.top.destroy()
 
-#mainframe is the parent class that holds all the other classes
-class MainFrame(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        """initilaises the main frame for tkinter app"""
-        tk.Tk.__init__(self, *args, **kwargs)
-        os.chdir(POLYCHRON_PROJECTS_DIR)
-        load_Window(self)
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
-        self.container = tk.Frame(self)
-        self.container.pack(side="top", fill="both", expand=True)
-        self.container.grid_rowconfigure(0, weight=1)
-        self.container.grid_columnconfigure(0, weight=1)
-        self.frames = {}
-
-
-    def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
-        frame.tkraise()
-        frame.config()
-
-    def get_page(self, page_class):
-        '''Shows the desired frame'''
-        return self.frames[page_class]
-
 class popupWindow4(object):
     def __init__(self, master, controller, resid_list, intru_list, node_track, graph):
         '''initialised window 4'''
