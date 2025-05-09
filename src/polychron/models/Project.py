25c6e29f5b36ff41c273b57232746203b1ceddd7
Peter Heywood <peethwd@gmail.com>
2025-05-09 14:17:02 +0100

    Implement Project.create_dirs

--- src/polychron/models/Project.py
+++ src/polychron/models/Project.py
@@ -1,4 +1,5 @@
 import pathlib
+import sys
 from dataclasses import dataclass, field
 
 from .Model import Model
@@ -19,3 +20,17 @@ class Project:
 
     models: dict[str, Model] = field(default_factory=dict)
     """A dictionary of models within this project, with their name as the key"""
+
+    def create_dirs(self) -> bool:
+        """Create the project directory.
+
+        Returns:
+            Boolean indicating success of directory creation
+        """
+        try:
+            # Ensure the model (and implictly project) directory exists
+            self.path.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            # @todo - better error handling. Should be due to permsissions, invalid filepaths or disk issues only
+            print(e, file=sys.stderr)
+            return False
